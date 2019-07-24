cf4f211b2fa2c176bc08eda4fce7fbf7281d2647
tshea113 <Tylershea12@gmail.com>
2019-07-23 23:36:39 -0500

    comment out unused thing

--- muskduino/muskduino.ino
+++ muskduino/muskduino.ino
@@ -89,14 +89,14 @@ void FillLEDsFromPaletteColors(uint8_t colorIndex, int brightness)
 //**************************************************************************************
 // Palette Setups
 //**************************************************************************************
-void rainbow_march(uint8_t thisdelay, uint8_t deltahue) {     // The fill_rainbow call doesn't support brightness levels.
-
-  uint8_t thishue = millis()*(255-thisdelay)/255;             // To change the rate, add a beat or something to the result. 'thisdelay' must be a fixed value.
+void rainbow_march(uint8_t thisdelay, uint8_t deltahue)       // The fill_rainbow call doesn't support brightness levels.
+{
+// uint8_t thishue = millis()*(255-thisdelay)/255;            // To change the rate, add a beat or something to the result. 'thisdelay' must be a fixed value.
   
-  thishue = beat8(50);                                       // This uses a FastLED sawtooth generator. Again, the '50' should not change on the fly.
+    uint8_t thishue = beat8(50);                              // This uses a FastLED sawtooth generator. Again, the '50' should not change on the fly.
 // thishue = beatsin8(50,0,255);                              // This can change speeds on the fly. You can also add these to each other.
   
-  fill_rainbow(leds, NUM_LEDS, thishue, deltahue);            // Use FastLED's fill_rainbow routine.
+    fill_rainbow(leds, NUM_LEDS, thishue, deltahue);          // Use FastLED's fill_rainbow routine.
 
 } // rainbow_march()
 
